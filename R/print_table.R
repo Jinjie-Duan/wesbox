d1231e077f4e783d3ab6fb99922c153fe26392f3
Jinjie Duan <jjduan@jinjies-imac.eduroam.net.au.dk>
2022-08-17 10:33:05 +0200

    add function kable_with_scroll_box

--- R/print_table.R
+++ R/print_table.R
@@ -16,3 +16,31 @@ kable_with_style <- function(table, ...) {
     kableExtra::kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive"))
   
 }
+
+
+#' Print table with scroll box 
+#'
+#' @param table A input table
+#' @param scroll.height The height param in scroll_box()
+#' @param scroll.width The width param in scroll_box()
+#' @param scroll.fixed_thead The fixed_thead param in scroll_box()
+#' @param ... Arguments from kableExtra::kable
+#'
+#' @return A styling table with scroll box
+#' @export
+#'
+#' @examples
+#' kable_with_scroll_box(iris)
+#' 
+kable_with_scroll_box <- function(table, scroll.height = "300px", scroll.width = "100%", scroll.fixed_thead = TRUE, ...) {
+  
+  kableExtra::kable(table, ...) %>%
+    kableExtra::kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive")) %>% 
+    kableExtra::scroll_box(height = scroll.height, width = scroll.width, fixed_thead = scroll.fixed_thead)
+
+}
+
+
+
+
+
